8488c3c6e16f808f8825a6745346cba45d26a622
rafff <ralmeih@gmail.com>
2023-05-04 04:48:46 +0200

    added lsp format

--- lua/user/lsp-zero.lua
+++ lua/user/lsp-zero.lua
@@ -11,8 +11,9 @@ lsp.on_attach(function(_, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>gt", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "<leader>gf", function() vim.lsp.buf.format() end, opts)
   vim.keymap.set("n", "<leader>gl", function() vim.diagnostic.open_float() end, opts) -- Works also without leader
 
   -- Rename
